da691775ee8837e7d00599d7a9ee92170a49329f
Zhenhui-HeYNWA <herbertynwa@outlook.com>
2025-05-09 02:09:42 +1000

    fix config

--- Tailwind.config.js
+++ Tailwind.config.js
@@ -3,10 +3,10 @@ import tailwindcssAnimate from 'tailwindcss-animate';
 
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './Components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+
+    './src/app/**/*.{js,ts,jsx,tsx}',
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class',
