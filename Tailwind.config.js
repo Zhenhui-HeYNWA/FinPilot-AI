16631d747de754a14a9ad6a557a0045c76fcd839
Zhenhui-HeYNWA <herbertynwa@outlook.com>
2025-05-09 02:43:59 +1000

    test

--- Tailwind.config.js
+++ Tailwind.config.js
@@ -10,6 +10,13 @@ const config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
       backgroundImage: {
         hero: 'url(/images/hero-bg.png)',
