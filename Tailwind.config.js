20b0eafe4c013fbf6020e3b71922ffb60a2a4bd3
Zhenhui-HeYNWA <herbertynwa@outlook.com>
2025-05-09 15:14:16 +1000

    test

--- Tailwind.config.js
+++ Tailwind.config.js
@@ -3,9 +3,9 @@ import tailwindcssAnimate from 'tailwindcss-animate';
 
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class',
